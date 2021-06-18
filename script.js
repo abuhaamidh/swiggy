8d20538939321a9aa05ce8f54c8fdf0c8329de2e
abuhaamidh <60191870+abuhaamidh@users.noreply.github.com>
2021-06-18 10:48:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var email= document.querySelector("#email");
+var pword= document.querySelector("#password").value;
+var pword1= document.querySelector("#password1").value;
+ 
+
+function validate(){
+    var pword= document.querySelector("#password").value;
+    var pword1= document.querySelector("#password1").value;
+    
+    if(pword1 pword){
+    alert("password doesn't match");          
+}
+}
